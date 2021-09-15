dea400e8574e10aec1af022c051579bdec33bdaa
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-09-15 11:42:24 +0900

    script_utilをnokogiriに移していく

--- script_util.py
+++ script_util.py
@@ -34,17 +34,31 @@ def prepare(prefix):
 def fname2id(fname: str) -> str:
     return str(fname).split("/")[-1].split(".")[0]
 
-def id2fname(_id, prefix="512white", ext=lambda _: "png", bucket=bucket):
-    return str(root/f"danbooru2020/{prefix}/{bucket(_id)}/{_id}.{ext(_id)}")
+def fname2prefix(fname: str) -> str:
+    return str(fname).split("/")[-3]
+
+def id2fname(_id, prefix="512white", ext="png", bucket=bucket, root=root):
+    _id = str(_id)
+    if not type(ext) == str:
+        ext = ext(_id)
+    if not type(bucket) == str:
+        bucket = bucket(_id)
+    return str(Path(root)/f"danbooru2020/{prefix}/{bucket}/{_id}.{ext}")
 
 class CurriedFunc:
-    def __init__(self, func):
+    def __init__(self, func, *args):
         self.func = func
-    @lru_cache(maxsize=None)
+        self.args = args
+    def __getattr__(self, arg):
+        return self.__getitem__(arg)
+    def __getitem__(self, arg):
+        return CurriedFunc(self.func, *self.args, arg)
     def __call__(self, *args, **kwargs):
-        return lambda x: self.func(x, *args, **kwargs)
-    def __getattr__(self, x):
-        return self.__call__(x)
+        return self.func(*args, *self.args, **kwargs)
+    def __str__(self):
+        return f"f({', '.join(['・']+list(self.args))})"
+    def __repr__(self):
+        return self.__str__()
 id2 = CurriedFunc(id2fname)
 
 def budget(N, workers=8):
@@ -59,16 +73,16 @@ def budget(N, workers=8):
         for a in arr:
             cumsum.append(cumsum[-1]+a)
         return f"<tr><td>{d}</td>"+"".join(f"<td>{{{s}..{e-1}}}</td>" for s, e in zip(cumsum[:-1], cumsum[1:]))+"</tr>"
-    display(HTML("<table>"+"\n".join(g(N, d) for d in range(1, workers+1))+"</table>"))
+    display(HTML("<table>"+"\n".join(g(N, d) for d in range(2, workers+1))+"</table>"))
 
-def split_dump(fnames, name, N=1000):
+def split_dump(fnames, name, N=1000, workers=8):
     if N <= 0:
         N = len(fnames)
     (root/f"danbooru2020/dump_{name}").mkdir(exist_ok=True)
     for i in range((len(fnames)-1)//N+1):
         with (root/f"danbooru2020/dump_{name}/{i}").open("w") as f:
             f.write('\n'.join(fnames[i*N:(i+1)*N])+"\n")
-    budget((len(fnames)-1)//N+1)
+    budget((len(fnames)-1)//N+1, workers=8)
 
 def search_dump(txt, name):
     for fname in tqdm(list((root/f"danbooru2020/dump_{name}/").glob("*"))):
@@ -87,84 +101,6 @@ class Filter(dict):
         super().__setitem__(k, v)
         i = list(self.keys()).index(k)
         print("{:>3}  {:>9,}  {}".format(i, len(v), k))
-
-class TQDMBytesReader(object):
-    def __init__(self, fd, **kwargs):
-        self.fd = fd
-        self.tqdm = tqdm(**kwargs)
-    def read(self, size=-1):
-        bytes = self.fd.read(size)
-        self.tqdm.update(len(bytes))
-        return bytes
-    def readline(self):
-        bytes = self.fd.readline()
-        self.tqdm.update(len(bytes))
-        return bytes
-    def __enter__(self):
-        self.tqdm.__enter__()
-        return self
-    def __exit__(self, *args, **kwargs):
-        return self.tqdm.__exit__(*args, **kwargs)
-def tqdm_load(fname):
-    with open(fname, "rb") as fd:
-         total = Path(fname).stat().st_size
-         with TQDMBytesReader(fd, total=total) as pbfd:
-             up = Unpickler(pbfd)
-             obj = up.load()
-    return obj
-
-
-class TQDMBytesReader(object):
-    def __init__(self, fd, **kwargs):
-        self.fd = fd
-        self.tqdm = tqdm(**kwargs)
-    def read(self, size=-1):
-        bytes = self.fd.read(size)
-        self.tqdm.update(len(bytes))
-        return bytes
-    def readline(self):
-        bytes = self.fd.readline()
-        self.tqdm.update(len(bytes))
-        return bytes
-    def __enter__(self):
-        self.tqdm.__enter__()
-        return self
-    def __exit__(self, *args, **kwargs):
-        return self.tqdm.__exit__(*args, **kwargs)
-
-def tqdm_load(fname):
-    with open(fname, "rb") as fd:
-         total = Path(fname).stat().st_size
-         with TQDMBytesReader(fd, total=total) as pbfd:
-             up = Unpickler(pbfd)
-             obj = up.load()
-    return obj
-
-
-class TQDMBytesReader(object):
-    def __init__(self, fd, **kwargs):
-        self.fd = fd
-        self.tqdm = tqdm(**kwargs)
-    def read(self, size=-1):
-        bytes = self.fd.read(size)
-        self.tqdm.update(len(bytes))
-        return bytes
-    def readline(self):
-        bytes = self.fd.readline()
-        self.tqdm.update(len(bytes))
-        return bytes
-    def __enter__(self):
-        self.tqdm.__enter__()
-        return self
-    def __exit__(self, *args, **kwargs):
-        return self.tqdm.__exit__(*args, **kwargs)
-def tqdm_load(fname):
-    with open(fname, "rb") as fd:
-         total = Path(fname).stat().st_size
-         with TQDMBytesReader(fd, total=total) as pbfd:
-             up = Unpickler(pbfd)
-             obj = up.load()
-    return obj
 
 class TQDMBytesReader(object):
     def __init__(self, fd, tqdm, total, desc=''):
