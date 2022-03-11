cc8b15e4a4fa1f2a37c6f32c1db9e070986eb772
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-03-11 23:23:42 +0900

    i2v_factory(torchvisionで前処理0.24sec/32batch)

--- script_i2v_factory.py
+++ script_i2v_factory.py
@@ -1,8 +1,9 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 device = "cuda"
 import pickle
 import torch
+import torchvision
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
@@ -18,18 +19,23 @@ with working_dir("/home/natsuki/illustration2vec"):
     import i2v.pytorch_i2v
     model_tag = i2v.pytorch_i2v.PytorchI2V(
                 "illust2vec_tag_ver200.pth", "tag_list.json").eval().requires_grad_(False).to(device)
+to224 = torchvision.transforms.Resize((224, 224))
+mean = torch.from_numpy(model_tag.mean[None,:,None,None]).float().to(device)
 
 query = "flandre scarlet"
 threshold=0.9
 skip = 0
 batch = 32
 sample_num = 1 * 10**7
+save_freq = 2500
 assert sample_num % batch == 0
+assert (sample_num//batch)%save_freq == 0
 label = torch.zeros([1, G.c_dim], device=device)
 root = Path(f"/data/natsuki/fact_{query.replace(' ', '_')}_{fm.parent.stem}")
 root.mkdir(exist_ok=True)
 pbar = tqdm(range(sample_num//batch), dynamic_ncols=True)
 ok = 0
+preds = list()
 for j in pbar:
     synth_image = G.synthesis(
         G.mapping(
@@ -39,14 +45,22 @@ for j in pbar:
         noise_mode='const'
     )
     synth_image = (synth_image + 1) * (255/2)
-    synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8).cpu().numpy()
-    imgs = [Image.fromarray(synth_image[i], 'RGB') for i in range(batch)]
-    pred = model_tag._estimate(imgs)
+    pros_image = to224(synth_image)[:,[2,1,0],:,:] - mean
+    pred = model_tag(pros_image)[:,:,0,0]
+    preds.append(pred.cpu().numpy())
     for i in range(batch):
         seed = batch*j+i+skip
-        pbar.set_description(f"{ok=} {skip=} {seed=} {sample_num=}")
         score = pred[i][model_tag.index[query]]
         if threshold < score:
-            txt = f"{score:.8f}_"[2:]+f"{seed}.png"
-            imgs[i].save(root/txt)
+            txt = f"{float(score):.8f}_"[2:]+f"{seed}.png"
+            Image.fromarray(
+                synth_image[i].permute(1, 2, 0).clamp(0, 255).to(torch.uint8).cpu().numpy(),
+                'RGB'
+            ).save(root/txt)
             ok += 1
+    pbar.set_description(f"{ok=} {seed=}")
+    if (j+1)%save_freq == 0:
+        print("saving...")
+        preds = [np.vstack(preds)]
+        with open(root/"preds.npy", "wb" ) as f:
+            np.save(f, preds[0])
