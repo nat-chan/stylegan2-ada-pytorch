1f40c5009ff76ee1f84801e77468ea512767bc4b
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-09-19 06:12:11 +0900

    i2vの推論した属性が0.9を上回っていたら(-score, seed)の要素のリストをヒープとしてpushし、pickleに保存する

--- script_i2v_factory.py
+++ script_i2v_factory.py
@@ -1,5 +1,6 @@
+# %%
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 device = "cuda"
 import pickle
 import torch
@@ -9,7 +10,9 @@ from pathlib import Path
 from tqdm import tqdm
 from PIL import Image
 from nokogiri.working_dir import working_dir
-
+from typing import List, Tuple
+import heapq # (-pred, seed)を保存していくヒープ
+# %%
 #fm = Path("/data/natsuki/training116/00030-v4-mirror-auto4-gamma100-batch64-noaug-resumecustom/network-snapshot-011289.pkl")
 fm = Path("/data/natsuki/training116/00023-white_yc05_yw04-mirror-auto4-gamma10-noaug/network-snapshot-021800.pkl")
 assert fm.is_file()
@@ -21,25 +24,45 @@ with working_dir("/home/natsuki/illustration2vec"):
                 "illust2vec_tag_ver200.pth", "tag_list.json").eval().requires_grad_(False).to(device)
 to224 = torchvision.transforms.Resize((224, 224))
 mean = torch.from_numpy(model_tag.mean[None,:,None,None]).float().to(device)
-
-query = "flandre scarlet"
+# %%
+root = Path(f"/data/natsuki/factory")
+root.mkdir(exist_ok=True)
+master: List[List[Tuple[float, int]]] = [list() for _ in model_tag.tags]
+def save(skip):
+    for k, tag in enumerate(model_tag.tags):
+        with open(root/f"{k}.pkl", "wb") as f:
+            pickle.dump(master[k], f)
+    with open(root/f"skip.txt", "w") as f:
+        f.write(str(skip))
+def lload():
+    try:
+        for k, tag in enumerate(model_tag.tags):
+            with open(root/f"{k}.pkl", "rb") as f:
+                master[k] = pickle.load(f)
+            with open(root/f"skip.txt", "r") as f:
+                skip = int(f.read())
+    except:
+        for k, tag in enumerate(model_tag.tags):
+            master[k] = list()
+        skip = 0
+    return skip
+skip = lload()
+# %%
 threshold=0.9
-skip = 0
 batch = 32
-sample_num = 1 * 10**7
-save_freq = 2500
+sample_num = 1 * 10**6
+#sample_num = 1 * 10**5
+#save_freq = 2500
+save_freq = 25
 assert sample_num % batch == 0
 assert (sample_num//batch)%save_freq == 0
 label = torch.zeros([1, G.c_dim], device=device)
-root = Path(f"/data/natsuki/fact_{query.replace(' ', '_')}_{fm.parent.stem}")
-root.mkdir(exist_ok=True)
 pbar = tqdm(range(sample_num//batch), dynamic_ncols=True)
-ok = 0
-preds = list()
-for j in pbar:
+#preds = list()
+for i in pbar:
     synth_image = G.synthesis(
         G.mapping(
-            torch.from_numpy(np.array([np.random.RandomState(batch*j+i+skip).randn(G.z_dim) for i in range(batch)])).to(device),
+            torch.from_numpy(np.array([np.random.RandomState(batch*i+j+skip).randn(G.z_dim) for j in range(batch)])).to(device),
             label
         ),
         noise_mode='const'
@@ -47,20 +70,17 @@ for j in pbar:
     synth_image = (synth_image + 1) * (255/2)
     pros_image = to224(synth_image)[:,[2,1,0],:,:] - mean
     pred = model_tag(pros_image)[:,:,0,0]
-    preds.append(pred.cpu().numpy())
-    for i in range(batch):
-        seed = batch*j+i+skip
-        score = pred[i][model_tag.index[query]]
-        if threshold < score:
-            txt = f"{float(score):.8f}_"[2:]+f"{seed}.png"
-            Image.fromarray(
-                synth_image[i].permute(1, 2, 0).clamp(0, 255).to(torch.uint8).cpu().numpy(),
-                'RGB'
-            ).save(root/txt)
-            ok += 1
-    pbar.set_description(f"{ok=} {seed=}")
-    if (j+1)%save_freq == 0:
-        print("saving...")
-        preds = [np.vstack(preds)]
-        with open(root/"preds.npy", "wb" ) as f:
-            np.save(f, preds[0])
+#    preds.append(pred.cpu().numpy())
+    for j in range(batch):
+        seed = batch*i+j+skip
+        for k, tag in enumerate(model_tag.tags):
+            score = float(pred[j][k])
+            if threshold < score:
+                heapq.heappush(master[k], (-score, seed))
+    pbar.set_description(f"skip={skip}, now={seed+1}")
+    if (i+1)%save_freq == 0:
+        print(f"saving...{sum(len(m) for m in master)}")
+        save(seed+1)
+#        preds = [np.vstack(preds)]
+#        with open(root/"preds.npy", "wb" ) as f:
+#            np.save(f, preds[0])
